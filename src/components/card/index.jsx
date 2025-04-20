8d425600e736597857802e0c2d2dae2c94539dbf
akmalxonov <117194536+akmalxonov@users.noreply.github.com>
2025-04-20 21:38:19 +0500

    end

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import '../card/card.scss';
 
-const Card = ({ name,desc,price,img, id, deleteTodo, editTodo }) => {
-    const [edit, setEdit] = useState(false); 
-    const [input, setInput] = useState(name);
+const Card = ({ name, desc, price, img, id, deleteTodo, editTodo }) => {
+    const [edit, setEdit] = useState(false);
+    const [newName, setName] = useState(name);
+    const [newPrice, setPrice] = useState(price);
+    const [newDesc, setDesc] = useState(desc);
+    const [newImg, setImg] = useState(img);
     const handleEdit = () => {
         if (edit) {
-            editTodo(id, input); 
+            editTodo(id, newName);
+            editTodo(id, newPrice);
+            editTodo(id, newDesc);
+            editTodo(id, newImg);
         }
         setEdit(!edit);
     };
@@ -14,19 +20,36 @@ const Card = ({ name,desc,price,img, id, deleteTodo, editTodo }) => {
     return (
         <div className='card'>
             {edit ? (
-                <input
-                    type="text"
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                />
+                <>
+                    <input
+                        type="text"
+                        value={newName}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        value={newPrice}
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        value={newDesc}
+                        onChange={(e) => setDesc(e.target.value)}
+                    />
+                    <input
+                        type="text"
+                        value={newImg}
+                        onChange={(e) => setImg(e.target.value)}
+                    />
+                </>
             ) : (
                 <div className="all">
-                    <p>{name}</p>
-                    <p>{desc}</p>
-                    <p>{price}</p>
-                    <img src={img} alt="img" />
+                    <p>{newName}</p>
+                    <p>{newPrice}</p>
+                    <p>{newDesc}</p>
+                    <img src={newImg} alt="img" />
                 </div>
-                
+
             )}
 
             <div className="btns">
--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -5,29 +5,36 @@ import '../form/form.scss'
 const Form = () => {
     useEffect(() => {
         axios.get("http://localhost:3000/products")
-            .then((res) => console.log(res.data))
+            .then((res) => setData(res.data))
+            .catch((err)=>console.log(err));
     }, [])
     const [name, setName] = useState("")
-    const [price,setPrice] = useState("")
-    const [desc,setDesc] = useState("")
-    const [img,setImg] = useState("")
+    const [price, setPrice] = useState("")
+    const [desc, setDesc] = useState("")
+    const [img, setImg] = useState("")
     const [data, setData] = useState([])
     const submit = (e) => {
         e.preventDefault()
         let newData = {
             id: Date.now(),
-            name: name
+            name,
+            price,
+            desc,
+            img
         }
         if (data.find(item => item.name === name)) return;
-        setData((p) => [    , ...p])
+        setData((p) => [newData, ...p])
         setName("")
+        setPrice("")
+        setDesc("")
+        setImg("")
         console.log(data);
 
     }
     const editTodo = (id, newText) => {
         const updatedData = data.map((item) => {
             if (item.id === id) {
-                return { ...item, name: newText };
+                return { ...item, name: newText , ...item,price:newText };
             }
             return item;
         });
@@ -41,10 +48,10 @@ const Form = () => {
         <div className='container'>
             <h1>salom</h1>
             <form onSubmit={submit} action="" className='form'>
-                <input type="name" onChange={(e) => setName(e.target.value)} value={name} />
-                <input type="price" onChange={(e) => setPrice(e.target.value)} value={price} />
-                <input type="desc" onChange={(e) => setDesc(e.target.value)} value={desc} />
-                <input type="img" onChange={(e) => setImg(e.target.value)} value={img} />
+                <input type="name" placeholder='Name' onChange={(e) => setName(e.target.value)} value={name} />
+                <input type="price" placeholder='Price' onChange={(e) => setPrice(e.target.value)} value={price} />
+                <input type="desc" placeholder='Description' onChange={(e) => setDesc(e.target.value)} value={desc} />
+                <input type="img" placeholder='Img' onChange={(e) => setImg(e.target.value)} value={img} />
                 <button>click</button>
             </form>
             {data.map((value) => {
